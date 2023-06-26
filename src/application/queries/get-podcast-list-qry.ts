064f5b625e82d52000a6ee056dc3ad8fdbf81823
mcabrera <manuel.cabrera@autentia.com>
2023-06-26 13:17:30 +0200

    implementación Local podcast repository

--- src/application/queries/get-podcast-list-qry.ts
+++ src/application/queries/get-podcast-list-qry.ts
@@ -1,15 +1,16 @@
 import { decorate, inject, injectable } from "inversify";
 import { TYPES } from "../../core/types/types";
-import { PodcastListDTO } from "../../domain/podcast/podcast-dto";
 import type { PodcastRepository } from "../../domain/podcast/podcast-repository";
 import { Query } from "../../domain/use-cases/query";
 import type { StateManager } from "../state-manager";
+import { PodcastList } from "../../domain/podcast/podcast";
+import { DtoToPodcastListTransform } from "../../infrastructure/dto-to-vtv-form.transform";
 
 decorate(injectable(), Query);
 
 
 @injectable()
-export class GetPodcastListQry extends Query<PodcastListDTO> {
+export class GetPodcastListQry extends Query<Promise<PodcastList>> {
 
   @inject(TYPES.STATE_MANAGER)
   private stateManager!: StateManager
@@ -17,13 +18,16 @@ export class GetPodcastListQry extends Query<PodcastListDTO> {
   @inject(TYPES.PODCAST_REPOSITORY)
   private podcastRepository!: PodcastRepository
 
+  private readonly dtoToVtvFormTransform: DtoToPodcastListTransform = new DtoToPodcastListTransform()
+
   constructor(
   ) {
     super();
   }
 
-  internalExecute(): PodcastListDTO {
-    console.log("",this.podcastRepository);
-    return this.stateManager.state.feed!;
+  async internalExecute(): Promise<PodcastList> {
+    const podcastList = await this.podcastRepository.getAll()
+    return podcastList
+    //return this.stateManager.state.feed!;
   }
 }
--- src/infrastructure/podcast-local-repository.ts
+++ src/infrastructure/podcast-local-repository.ts
@@ -1,14 +1,24 @@
 import { injectable } from "inversify";
 import { PodcastRepository } from "../domain/podcast/podcast-repository";
 import { PodcastListDTO } from "../domain/podcast/podcast-dto";
+import dataPodCastList from '../core/dataPodcastList.json';
+import { PodcastList } from "../domain/podcast/podcast";
+import { DtoToPodcastListTransform } from "./dto-to-vtv-form.transform";
+
 
 @injectable()
 export class PodcastLocalRepository implements PodcastRepository {
-  getAll(): Promise<PodcastListDTO> {
-    console.log("GET_ALLL");
-    throw new Error("Method not implemented.");
+  getAll(): Promise<PodcastList> {
+
+    const dtoToPodcastListTransform: DtoToPodcastListTransform = new DtoToPodcastListTransform()
+    const data = dataPodCastList
+
+    const podcastListDTO: PodcastListDTO = data.feed
+
+    return Promise.resolve(dtoToPodcastListTransform.transform(podcastListDTO))
+
   }
-  findById(): Promise<PodcastListDTO> {
+  findById(): Promise<PodcastList> {
     throw new Error("Method not implemented.");
   }
 }
